c30bd288c736a96518732344a4b348b5159f24c6
HAPPI <152979599+Abbas02-02@users.noreply.github.com>
2025-11-20 16:23:05 +0530

    Update Streamlit_app.py
    Pandas added

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -19,7 +19,12 @@ cnx = st.connection("Snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#convert snowflake dataframe to panda datafram
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
